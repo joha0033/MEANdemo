f704b5993c37ef57834a8af34a11a7fb7509795c
Austin <joha0033@gmail.com>
2017-10-23 13:51:01 -0600

    added more routes, and working

--- app.js
+++ app.js
@@ -7,11 +7,26 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+const users = require('./routes/users.js')
+
+//port number
 const port = 3000
 
-app.get('/', (req, res) => {
+//cors middleware
+app.use(cors())
+
+//bodyparser middleware
+app.use(bodyParser.json())
+
+
+app.use('/users', users)
+
+//index route
+app.get('/', (req, res, next) => {
   res.send('what up? who are you?')
 })
+
+//start server
 app.listen(port, () => {
   console.log('serve me P3000');
 })
