8817746d909bd15abb4b2916ca884f600a22bd54
Austin <joha0033@gmail.com>
2017-10-23 14:05:40 -0600

    mongoose db set up

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const passport = require('passport')
 const mongoose = require('mongoose')
+const config = require('./config/database')
+
+//Connect to Database
+mongoose.connect(config.database)
+
+
+//On the connection
+mongoose.connection.on('connected', ()=> {
+  console.log('connected to', config.database, 'motherfucker');
+})
+mongoose.connection.on('error', (err)=> {
+  console.log('NOT connected to', config.database, ', motherfucker', err);
+})
+
 
 const app = express()
 
-const users = require('./routes/users.js')
+const users = require('./routes/users')
 
 //port number
 const port = 3000
@@ -18,6 +32,8 @@ app.use(cors())
 //bodyparser middleware
 app.use(bodyParser.json())
 
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/users', users)
 
