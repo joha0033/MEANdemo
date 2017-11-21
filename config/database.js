af75080b489d24ea3b8021c8ee307b0e2ef6d35f
Austin <joha0033@gmail.com>
2017-11-21 15:52:52 -0700

    updated public folder

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
-  // database: 'mongodb://joha0033:usa20034@ds031835.mlab.com:31835/grateful-bound',
+  // database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://joha0033:usa20034@ds031835.mlab.com:31835/grateful-bound',
   secret: 'secret'
 }
